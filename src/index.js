143c356bd312b94a13d7a3e400ff7ec4a0d09e5a
vifi-on <ladiasman1992@gmail.com>
2021-02-10 15:48:45 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+  const posNum = n < 0 ? n * -1 : n
+  const arr = Array.from(String(posNum))
+  const reverseArr = arr.reverse()
+  const result = Number(reverseArr.join(''))
   
+  return result
 }
+
